977f1edce9f5848e7d4012c7635959dbf0c681d0
Minghui Sun <smh335789877@gmail.com>
2019-08-31 22:12:05 +0800

    usrInfo back function
    store user information into db

--- pages/index/index.js
+++ pages/index/index.js
@@ -14,10 +14,44 @@ Page({
     })
   },
   onLoad: function () {
+    var openid
     wx.cloud.callFunction({
       name: 'login',
       complete: res => {
-        console.log('callFunction test result: ', res)
+        //console.log('callFunction test result: ', res.result)
+        openid = res.result.openid
+      }
+    })
+    wx.getUserInfo({
+      success: function (res){
+        var logged
+        var information = res.userInfo
+        const db = wx.cloud.database()
+        const userInfo = db.collection('usr_information')
+        // console.log('userInfo is : ', information)
+        userInfo.where({ openid: openid }).get({
+          success: function (res) {
+            //console.log(res.data)
+            //console.log(res.data.length)
+            logged = res.data.length
+            //console.log(logged)
+            if (logged == 0) {
+              userInfo.add({
+                data: {
+                  Location: information.city,
+                  Gender: information.gender,
+                  Openid: openid
+                },
+                success: function (res) {
+                  // res 是一个对象，其中有 _id 字段标记刚创建的记录的 id
+                  //console.log("data is" ,res)
+                },
+                fail: console.error
+              })
+            }
+          }
+        })
+        
       }
     })
   }
