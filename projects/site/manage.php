4c43b4969903071bbccc86b9e65651e7b641306b
Max Chatfield <maxc7.08@gmail.com>
2019-09-25 00:18:55 +0800

    yeee finally got it working

--- projects/site/manage.php
+++ projects/site/manage.php
@@ -11,6 +11,10 @@ else
 	echo "<font color = #FF0000> session failed. </font>";
 }
 
+// later this should fetch it from the existing questionaire if there is.
+$_SERVER["group_num"] = 1;
+$_SERVER["group_ratio"] = 1;
+$_SERVER["question_num"] = 3;
 ?>
 
 <!DOCTYPE html>
@@ -25,10 +29,12 @@ body
 <body>
 <h1>Manage Questionaire.</h1>
 <form method = "post" action = "<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
-How many groups: <input type = "text" name = "group_num"> <br>
+How many groups: <input type = "number" name = "group_num"> <br>
 Group ratio: <input type = "text" name = "group_ratio"> <br>
+How many questions: <input type = "number" name = "question_num"> <br>
 Submit: <input type = "submit" value = "Submit">
 </form>
+
 <?php 
 $groups = 0;
 $ratio = "";
@@ -37,9 +43,42 @@ if ($_SERVER["REQUEST_METHOD"] == "POST")
 {
 	if (isset($_POST["group_num"]) && isset($_POST["group_ratio"]))
 	{
-		echo $_SESSION["user_name"];
+		$_SERVER["group_num"] = $_POST["group_num"];
+		$_SERVER["group_ratio"] = $_POST["group_ratio"];
+	}
+	
+	if (isset($_POST["question_num"]))
+	{
+		$_SERVER["question_num"] = $_POST["question_num"];
 	}
 }
 ?>
+<div>
+<h2> Questions </h2>
+<form>
+<?php
+for ($i = 1; $i <= $_SERVER["question_num"]; $i++)
+{ 
+	echo "<h3>question $i </h3>";
+	
+	// belong to which groups
+	echo "Group Selection: ";
+	for ($j = 1; $j <= $_SERVER["group_num"]; $j++)
+	{
+		echo "<input type = \"checkbox\" name = \"question_".$i."_group_$j value = \"$j\" checked> Group $j ";
+	}
+	echo "<br>";
+	
+	// question types
+	echo "<input type = \"radio\" name = \"question_type$i\" value = \"short_answer\" checked>Short Answer ";
+	echo "<input type = \"radio\" name = \"question_type$i\" value = \"radio\">radio ";
+	
+	echo "<br>";
+	// question itself
+	echo "<textarea name = \"question_value$i\" rows = \"5\" cols = \"40\"></textarea>";
+}
+?>
+</form>
+</div>
 </body>
 </html>
